131e9008c4997db9519bade1a991166bf6047caa
Pyeonno <pyeonno13634@gmail.com>
2025-10-20 10:02:24 +0900

    fix test rbtree

--- src/rbtree.c
+++ src/rbtree.c
@@ -11,14 +11,10 @@ rbtree *new_rbtree(void) {
 }
 
 void nodeTreeDelete(rbtree *t, node_t *cur) {
-  if (cur == t->nil) {
-    return;
-  }
+  if (cur == t->nil) { return; }
   nodeTreeDelete(t, cur->left);
   nodeTreeDelete(t, cur->right);
-
   free(cur);
-  cur = NULL;
 }
 
 void delete_rbtree(rbtree *t) {
@@ -33,23 +29,62 @@ void delete_rbtree(rbtree *t) {
 }
 
 node_t *rbtree_insert(rbtree *t, const key_t key) {
+  node_t *parent = t->nil;
+  node_t *p = t->root;
+  while (p != t->nil){
+      parent = p;
+      if (key < p -> key){
+          p = p->left;
+      }
+      else{
+          p = p->right;
+      }
+  }
+
+  node_t *new_node = malloc(sizeof(node_t));
+  new_node->key = key;
+  new_node->left = t->nil;
+  new_node->right = t->nil;
+  new_node->color = RBTREE_RED;
+  new_node->parent = parent;
+
+  if (parent == t->nil)
+      t->root = new_node;
+  else if (key < parent->key)
+      parent->left = new_node;
+  else
+      parent->right = new_node;
+  
+  
 
   return t->root;
 }
 
 node_t *rbtree_find(const rbtree *t, const key_t key) {
-  // TODO: implement find
-  return t->root;
+  node_t *cur = t->root;
+  while (cur != t->nil)
+  {
+    if (key == cur->key)
+      return cur;
+    else
+      cur = (key < cur->key) ? cur->left : cur->right;
+  }
+  return NULL;
 }
 
 node_t *rbtree_min(const rbtree *t) {
-  // TODO: implement find
-  return t->root;
+  node_t *cur = t->root;
+
+  while (cur->left != t->nil)
+    cur = cur->left;
+  return cur;
 }
 
 node_t *rbtree_max(const rbtree *t) {
-  // TODO: implement find
-  return t->root;
+  node_t *cur = t->root;
+  while (cur->right != t->nil)
+    cur = cur->right;
+  return cur;
 }
 
 int rbtree_erase(rbtree *t, node_t *p) {
